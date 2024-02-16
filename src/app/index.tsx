a46c503f17bdf62c533aab33be2fc3f50e5a3627
EsdrasCFF <ecff.dev@gmail.com>
2024-02-16 16:22:04 -0300

    feat: initial configs and project setup

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View className='bg-slate-600 flex-1' >
+      <Text className='text-white' > Hello! </Text>
+    </View>
+  )
+}
